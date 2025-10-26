233c5884e1cf7a58348cfe20cdd59e73f4dc35cb
Sibitenda <hsibitenda@umu.ac.ug>
2025-10-26 18:15:05 +0300

    Fix: add corsheaders and gunicorn to requirements

--- school_project/urls.py
+++ school_project/urls.py
@@ -1,34 +1,3 @@
-# from django.contrib import admin
-# from django.urls import path, include
-# # urlpatterns = [
-# #     path("admin/", admin.site.urls),
-# #     path("", include("reports.urls")),  # all app routes handled inside reports/urls.py
-# # ]
-# from django.contrib import admin
-# from django.urls import path, include
-# from django.conf import settings
-# from django.conf.urls.static import static
-
-# urlpatterns = [
-#     path("admin/", admin.site.urls),
-#     path("", include("reports.urls")),   # your app routes
-#     path("accounts/", include("django.contrib.auth.urls")),  # ✅ login/logout
-# ]
-
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# from django.contrib import admin
-# from django.urls import path, include
-# from django.shortcuts import redirect
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('dashboard/', include('reports.urls')),  # replace 'yourapp' with your app name
-#     path('accounts/', include('django.contrib.auth.urls')),
-
-#     # Redirect root URL to dashboard
-#     path('', lambda request: redirect('dashboard')),
-# ]
 from django.contrib import admin
 from django.urls import path, include
 
